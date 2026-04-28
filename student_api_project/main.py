5fe1f5cc133ba46a2e83df9429987fee7162c0f9
Saloni <shaluchaudhary6398532579@gmail.com>
2026-04-28 09:22:35 +0530

    My first commit

---
+++ student_api_project/main.py
@@ -0,0 +1,46 @@
+from Fastapi import fastapi 
+import os
+import json
+import student_api_project
+
+app = fastapi()
+
+file_name = "students.json"
+
+#Read data from file
+
+if os.path.exists(file_name):
+   with open (file_name,"r") as f :
+        f.read()
+        print(json.load)
+    
+#Write data to file
+
+with open(file_name,"w")as f:
+    f.write()
+    print(json.load)
+
+
+# 3. FastAPI Endpoints (20 Marks)
+# Create following APIs:
+
+@app.get("/")
+def home():
+ return{"message":"Welcome to Student API"}
+
+@app.get("/students")
+def Get_All_Students():
+   return all 
+
+@app.post("/students{id}")
+def post():
+   return {"message":"post to Student API" }
+
+@app.get("/students/{id}")
+def search():
+   return {"message":"search to Student API"}
+
+@app.get("DELETE /students/{id}")
+def delete():
+   return {"message":"delete to Student API"}
+
